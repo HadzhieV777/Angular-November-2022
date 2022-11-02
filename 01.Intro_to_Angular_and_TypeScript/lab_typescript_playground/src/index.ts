3e369dfa79c31393c8048efc1c377aeea07ad2e6
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-02 14:44:54 +0200

    Added src dir

---
+++ 01.Intro_to_Angular_and_TypeScript/lab_typescript_playground/src/index.ts
@@ -0,0 +1,15 @@
+// 01. Data Class
+class Data {
+  constructor(
+    public method: string,
+    public uri: string,
+    public version: string,
+    public message: string,
+    public response: string = 'undefined',
+    public fulfilled: boolean = false,
+  ) {}
+}
+
+
+let myData = new Data('GET','http://google.com', 'HTTP/1.1', '')
+console.log(myData)
