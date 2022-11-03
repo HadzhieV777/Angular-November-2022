504057ebd190071b4d9769b33fe346329ddc8592
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-03 13:47:16 +0200

    Edited index.ts

--- 01.Intro_to_Angular_and_TypeScript/lab_typescript_playground/src/index.ts
+++ 01.Intro_to_Angular_and_TypeScript/lab_typescript_playground/src/index.ts
@@ -1,5 +1,6 @@
 import Data from "./01.data_class"
 import ticketManager from "./02.tickets"
+import Box from "./05.boxes"
 // 01. Data Class
 
 // let myData = new Data('GET','http://google.com', 'HTTP/1.1', '')
@@ -20,6 +21,18 @@ import ticketManager from "./02.tickets"
 // 04.Elemelons
 
 // 05.Boxes
+// let box = new Box<Number>();
+// box.add(1);
+// box.add(2);
+// box.add(3);
+// console.log(box.count);
+
+// let box = new Box<String>();
+// box.add("Pesho");
+// box.add("Gosho");
+// console.log(box.count);
+// box.remove();
+// console.log(box.count);
 
 // 06.KeyValuePairs
 
