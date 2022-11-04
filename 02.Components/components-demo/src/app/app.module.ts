c904d60e954c4bbdfd698d684b5b06ad777097a1
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-04 13:31:21 +0200

    Edited app.module.ts

--- 02.Components/components-demo/src/app/app.module.ts
+++ 02.Components/components-demo/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
