1be9d1fcbae4e4616f711b7edf0d7b8732014eb2
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-04 14:15:21 +0200

    Edited list.component.ts

--- 02.Components/components-demo/src/app/list/list.component.ts
+++ 02.Components/components-demo/src/app/list/list.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit {
-
+export class ListComponent {
+  myProp =  'Hello World!'
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
