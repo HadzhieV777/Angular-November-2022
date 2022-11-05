655245f6588b45b42985060f96ea75e7ed6ed2b4
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-05 09:20:20 +0200

    Edited app.component.ts

--- 02.Components/components-demo/src/app/app.component.ts
+++ 02.Components/components-demo/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'components-demo';
+
+  changeTitleHandler(newTitle: string) {
+    this.title = newTitle;
+  }
 }
