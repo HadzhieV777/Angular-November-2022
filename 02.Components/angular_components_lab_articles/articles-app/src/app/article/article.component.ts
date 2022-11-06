82f3c09812170b3f138c51ca86f840a5ebb4d249
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-06 14:11:45 +0200

    Added readMore method

--- 02.Components/angular_components_lab_articles/articles-app/src/app/article/article.component.ts
+++ 02.Components/angular_components_lab_articles/articles-app/src/app/article/article.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Article } from '../models/article.model';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.scss']
+  styleUrls: ['./article.component.scss'],
 })
-export class ArticleComponent implements OnInit {
+export class ArticleComponent {
+  private symbols: number = 250;
 
-  constructor() { }
+  // using @Input decorator bcs
+  // article and aricleDesc will come from above in the articles component
+  @Input() article: Article;
+  @Input() articleDesc: string;
+  descToShow: string;
 
-  ngOnInit(): void {
+  // articleDescLen is a counter which controls how many symbols to show
+  articleDescLen: number;
+  showReadMoreBtn: boolean = true;
+  showHideBtn: boolean = false;
+  imageIsShown: boolean = false;
+  imageButtonTitle: string = 'Show Image';
+
+  // this can also be done in the ngOnInit lifecycle hook
+  constructor() {
+    this.articleDescLen = 0;
+    this.descToShow = '';
+  }
+
+  readMore(): void {
+    this.articleDescLen += this.symbols;
+    if (this.articleDescLen >= this.articleDesc.length) {
+      this.showHideBtn = true;
+      this.showReadMoreBtn = false;
+    } else {
+      this.descToShow = this.articleDesc.substring(0, this.articleDesc)
+    }
   }
 
 }
+
+// ng g c component-name
+//    - generates new component
+//    - creates all the component files
+//    - imports the component in declarations array inside main app modules
