0f0cb3a5f1ce09bdbb625d27f6f2dd12356cc7f3
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-06 12:53:19 +0200

    Edited seed.ts

--- 02.Components/angular_components_lab_articles/articles-app/src/app/data/seed.ts
+++ 02.Components/angular_components_lab_articles/articles-app/src/app/data/seed.ts
@@ -17,4 +17,10 @@ const data = [
         author: 'Author 3',
         imageUrl: 'http://i.dailymail.co.uk/i/pix/2010/04/17/article-1266852-092DE58A000005DC-112_634x411.jpg'
     },
+    {
+        title: 'Article 4',
+        description: 'LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1v',
+        author: 'Author 4',
+        imageUrl: 'https://www.seoclerk.com/pics/518476-1iDFBR1489198900.png'
+    },
 ]
