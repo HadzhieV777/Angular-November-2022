7662e4bb23c4b350ff2d5dbbceab66a820c8a68a
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-07 12:43:55 +0200

    Edited app.module.ts

--- 02.Components/angular_components_lab_articles/articles-app/src/app/app.module.ts
+++ 02.Components/angular_components_lab_articles/articles-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+import { ArticlesComponent } from './articles/articles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule
