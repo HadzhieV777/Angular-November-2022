d3228ad10b4fe47929dcd184fd69d020fc474394
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-08 20:51:47 +0200

    Edited app.module.ts

--- 03.DI_RxJS_Services/demo/di-cd-obs/src/app/app.module.ts
+++ 03.DI_RxJS_Services/demo/di-cd-obs/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, Provider } from '@angular/core';
+import { InjectionToken, NgModule, Provider } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -10,10 +10,17 @@ export class MyClass {
   }
 }
 
+// The token should but does not need to be unique
+// we can use the token to provide a value
+// InjectionToken is like JS Symbols bcs Symbol('asd') !== Symbol('asd')
+// Symbols are often used to add unique property keys to an object
+export const myCustomToken = new InjectionToken('Test');
+
 const myProvider: Provider = {
   // useValue: 123,
   useClass: MyClass,
-  provide: MyClass, // key of the provider
+  provide: myCustomToken,
+  // provide: MyClass, // key of the provider
 };
 
 @NgModule({
@@ -28,9 +35,8 @@ const myProvider: Provider = {
 })
 export class AppModule {}
 
-
-// You must register at least one provider of any service you are going to use. 
-// The provider can be part of the service's own metadata, making that service available everywhere, 
-// or you can register providers with specific modules or components. 
-// You register providers in the metadata of the service (in the @Injectable() decorator), 
+// You must register at least one provider of any service you are going to use.
+// The provider can be part of the service's own metadata, making that service available everywhere,
+// or you can register providers with specific modules or components.
+// You register providers in the metadata of the service (in the @Injectable() decorator),
 // or in the @NgModule() or @Component() metadata
