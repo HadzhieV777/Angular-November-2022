8b24a61943c950f6e0cb7d2c571f6cb501891220
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-08 20:20:54 +0200

    Edited app.module.ts

--- 03.DI_RxJS_Services/demo/di-cd-obs/src/app/app.module.ts
+++ 03.DI_RxJS_Services/demo/di-cd-obs/src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Provider } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 
+const myProvider: Provider = {
+  useValue: 123,
+  provide: 'test', // key of the provider
+};
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    TestComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TestComponent],
+  imports: [BrowserModule],
   // Inject the dependencies here
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [myProvider],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
