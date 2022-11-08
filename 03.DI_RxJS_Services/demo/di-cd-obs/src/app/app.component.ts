3e68402909c5abc64259e6c3bef778462485593d
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-08 14:35:42 +0200

    Edited app.component.ts

--- 03.DI_RxJS_Services/demo/di-cd-obs/src/app/app.component.ts
+++ 03.DI_RxJS_Services/demo/di-cd-obs/src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'di-cd-obs';
+  counter = 0;
+  constructor() {
+    // setInterval must contain arrow function, otherwise we will lose the context
+    setInterval(() => {
+      this.counter++;
+    }, 3000);
+  }
 }
