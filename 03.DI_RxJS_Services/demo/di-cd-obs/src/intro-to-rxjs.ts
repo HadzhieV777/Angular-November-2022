3ac6b8abb8c7c9c7c8c193302351ef2327330279
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-09 14:58:09 +0200

    Edited intro-to-rxjs.ts

--- 03.DI_RxJS_Services/demo/di-cd-obs/src/intro-to-rxjs.ts
+++ 03.DI_RxJS_Services/demo/di-cd-obs/src/intro-to-rxjs.ts
@@ -9,6 +9,7 @@
 // When a Promise object is "fulfilled", the result is a value.
 // When a Promise object is "rejected", the result is an error object.
 
+// Promises are like async arrays
 function getValue() {
   return new Promise((res) => {
     setTimeout(() => {
@@ -16,3 +17,25 @@ function getValue() {
     }, 2000);
   });
 }
+
+function getValue2(cb: (...args: any[]) => void) {
+  setTimeout(() => {
+    cb('Test');
+  }, 2000);
+}
+
+// cps
+getValue2(function (value) {
+  console.log(value);
+});
+
+// promise chain
+getValue()
+  .then(function (value) {
+    console.log(value);
+  })
+  .then()
+  .then();
+
+// The difference between array and promise is that in the promise the stored value is uknown when the promise is created
+// and the value can be mutated when returned
