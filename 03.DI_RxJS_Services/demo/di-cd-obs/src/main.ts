777951dc994f3a008df044625622c25d6a551345
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-09 14:01:42 +0200

    Edited main.ts

--- 03.DI_RxJS_Services/demo/di-cd-obs/src/main.ts
+++ 03.DI_RxJS_Services/demo/di-cd-obs/src/main.ts
@@ -10,3 +10,6 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+// main.ts can be used if we will implement functionality to our app before the app starts
