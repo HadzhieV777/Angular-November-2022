7dee91dc1b38dde65626e35b1f39d4d76773f5fa
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-09 11:34:23 +0200

    Edited test.component.ts

--- 03.DI_RxJS_Services/demo/di-cd-obs/src/app/test/test.component.ts
+++ 03.DI_RxJS_Services/demo/di-cd-obs/src/app/test/test.component.ts
@@ -2,16 +2,23 @@ import {
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
+  Injector,
   Input,
   OnChanges,
   OnInit,
   SimpleChanges,
 } from '@angular/core';
+import { AppComponent } from '../app.component';
+import { myCustomToken } from '../app.module';
 
+// The service has lifecycle hooks inside
+// ngOnDestroy is the only one lifecycle hook that is inside the service
+// because we can destroy services
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.scss'],
+  providers: [],
 
   // Good for opt
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,11 +26,17 @@ import {
 export class TestComponent implements OnInit, OnChanges {
   @Input() users!: { name: string }[];
 
-  constructor(private cdRef: ChangeDetectorRef) {
+  constructor(private cdRef: ChangeDetectorRef, private injector: Injector) {
     // Detaches this view from the change-detection tree.
     // A detached view is not checked until it is reattached.
     // Use in combination with detectChanges() to implement local change detection checks.
     this.cdRef.detach();
+    const value = this.injector.get(myCustomToken);
+
+    // AppComponent is accessible bcs it was created up on the tree
+    const appCmp = this.injector.get(AppComponent);
+    console.log(value);
+    console.log(appCmp);
   }
 
   ngOnChanges(): void {
@@ -47,7 +60,7 @@ class Wallet {
 }
 
 class Person {
-  constructor (private wallet: Wallet) {
+  constructor(private wallet: Wallet) {
     // this.wallet = new Wallet(200);
     // Wrong bcs we interrupt the Dependency Inversion pattern
   }
@@ -56,3 +69,6 @@ class Person {
 // const w = new Wallet(200, 'test');
 // const p = new Person(w);
 
+
+// In AngularJS, a service is a function, or object, that is available for, and limited to, your AngularJS application.
+// AngularJS has about 30 built-in services. One of them is the $location service.
