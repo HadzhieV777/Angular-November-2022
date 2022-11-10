042a797e49756c47ad61d37d06b900d394702677
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-10 13:20:48 +0200

    Edited app.component.ts

--- 03.DI_RxJS_Services/demo/di-cd-obs/src/app/app.component.ts
+++ 03.DI_RxJS_Services/demo/di-cd-obs/src/app/app.component.ts
@@ -6,8 +6,6 @@ import { MyClass, myCustomToken } from './app.module';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-
-
 export class AppComponent {
   title = 'di-cd-obs';
   // counter = 0;
@@ -15,13 +13,12 @@ export class AppComponent {
   users = [{ name: 'Pesho' }, { name: 'Gosho' }];
 
   // @Inject is Parameter decorator on a dependency parameter of a class constructor that specifies a custom provider of the dependency.
-  constructor(
-    // @Inject('Test') test: string
-   @Optional() @Inject(myCustomToken) test: string,
-    // @Inject(MyClass) test: MyClass =>  
-    // test: MyClass
-  ) {
-    console.log(test);
+  constructor() // @Inject('Test') test: string
+  //  @Optional() @Inject(myCustomToken) test: string,
+  // @Inject(MyClass) test: MyClass =>
+  // test: MyClass
+  {
+    // console.log(test);
     // setInterval must contain arrow function, otherwise we will lose the context
     //   setInterval(() => {
     //     this.counter++;
