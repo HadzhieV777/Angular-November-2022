e1c82b0ac144751d9a3c64f9ca910d7e8f345f23
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-11 20:45:47 +0200

    Edited app.module.ts

--- Softuni_Forum/forum/src/app/app.module.ts
+++ Softuni_Forum/forum/src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CoreModule,
+    // AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
