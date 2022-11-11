0c009ab37a21c2d74cf085dbc23b8542f2b52789
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-11 20:46:04 +0200

    Edited core.modules.ts

--- Softuni_Forum/forum/src/app/core/core.module.ts
+++ Softuni_Forum/forum/src/app/core/core.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent],
+  imports: [CommonModule],
+  exports: [HeaderComponent], // Only for declarations members
 })
-export class CoreModule { }
+export class CoreModule {}
