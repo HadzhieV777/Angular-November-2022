4617bc4a572571f0715f19d9467df6fb891c1ae4
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-11 20:56:01 +0200

    Generated recent-posts component

---
+++ Softuni_Forum/forum/src/app/recent-posts/recent-posts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recent-posts',
+  templateUrl: './recent-posts.component.html',
+  styleUrls: ['./recent-posts.component.scss']
+})
+export class RecentPostsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
