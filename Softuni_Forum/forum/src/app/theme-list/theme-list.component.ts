54172446cf3922dcb1d661b81e33fd4905ab4e4e
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-11 20:56:31 +0200

    Generated theme-list component

---
+++ Softuni_Forum/forum/src/app/theme-list/theme-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-list',
+  templateUrl: './theme-list.component.html',
+  styleUrls: ['./theme-list.component.scss']
+})
+export class ThemeListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
