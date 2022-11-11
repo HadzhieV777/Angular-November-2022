8072621862361559fe6faedf93415bbce9671fef
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-11 20:55:09 +0200

    Edited app.module.ts

--- Softuni_Forum/forum/src/app/app.module.ts
+++ Softuni_Forum/forum/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 // import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { ThemeListComponent } from './theme-list/theme-list.component';
+import { RecentPostsComponent } from './recent-posts/recent-posts.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ThemeListComponent, RecentPostsComponent],
   imports: [
     BrowserModule,
     CoreModule,
