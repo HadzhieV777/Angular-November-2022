0f17eca5d660380acdad99e92c05e4e14b926b68
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-12 09:15:31 +0200

    Edited theme-list.component.ts

--- Softuni_Forum/forum/src/app/theme-list/theme-list.component.ts
+++ Softuni_Forum/forum/src/app/theme-list/theme-list.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiService } from '../api.service';
 @Component({
   selector: 'app-theme-list',
   templateUrl: './theme-list.component.html',
-  styleUrls: ['./theme-list.component.scss']
+  styleUrls: ['./theme-list.component.scss'],
 })
 export class ThemeListComponent implements OnInit {
-
-  constructor() { }
+  constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
+    this.apiService.loadThemes().subscribe((value) => {
+      console.log(value);
+    });
   }
-
 }
