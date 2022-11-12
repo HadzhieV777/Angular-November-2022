bb60702a89460bd2e8c6af312a9d588466ec4fa4
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-12 09:14:47 +0200

    Edited recent-posts.component.ts

--- Softuni_Forum/forum/src/app/recent-posts/recent-posts.component.ts
+++ Softuni_Forum/forum/src/app/recent-posts/recent-posts.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-recent-posts',
   templateUrl: './recent-posts.component.html',
-  styleUrls: ['./recent-posts.component.scss']
+  styleUrls: ['./recent-posts.component.scss'],
 })
 export class RecentPostsComponent implements OnInit {
-
-  constructor() { }
+  constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
+    this.apiService.loadPosts().subscribe((value) => {
+      console.log(value);
+    });
   }
-
 }
