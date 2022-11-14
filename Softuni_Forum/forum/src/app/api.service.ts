a82cfaeb1e3dd9a7ab59c2a372f8871cd1273f4c
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-14 13:41:40 +0200

    Edited api.service.ts

--- Softuni_Forum/forum/src/app/api.service.ts
+++ Softuni_Forum/forum/src/app/api.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
+import { ITheme } from './interfaces/theme';
+import { IPost } from './interfaces/post';
 const apiUrl = environment.apiUrl;
 
 @Injectable({
@@ -10,11 +12,11 @@ export class ApiService {
   constructor(private httpClient: HttpClient) {}
 
   loadThemes() {
-    return this.httpClient.get(`${apiUrl}/themes`);
+    return this.httpClient.get<ITheme[]>(`${apiUrl}/themes`);
   }
 
   loadPosts(limit?: number) {
-    return this.httpClient.get(
+    return this.httpClient.get<IPost[]>(
       `${apiUrl}/posts${limit ? `?limit=${limit}` : ''}`
     );
   }
