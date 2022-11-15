e50cf751fae6c7a185b8dd780a99e2b7d28e8bf8
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-15 21:17:42 +0200

    Generated shared module

---
+++ 05.Modules_and_Routing/modules-and-routing-demo/src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SpinnerComponent } from './spinner/spinner.component';
+
+
+
+@NgModule({
+  declarations: [
+    SpinnerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
