599f95767dc49b8d4ccee3ca32380c4a53fec225
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-16 09:26:24 +0200

    Edited shared.module.ts

--- 05.Modules_and_Routing/modules-and-routing-demo/src/app/shared/shared.module.ts
+++ 05.Modules_and_Routing/modules-and-routing-demo/src/app/shared/shared.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from './spinner/spinner.component';
 
-
-
 @NgModule({
-  declarations: [
-    SpinnerComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [SpinnerComponent],
+  imports: [CommonModule],
+  exports: [SpinnerComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
