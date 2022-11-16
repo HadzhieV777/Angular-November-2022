4d0226a43c727b0d7359025ab1d9d50f42a431fd
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-16 09:23:26 +0200

    Edited app.module.ts

--- 05.Modules_and_Routing/modules-and-routing-demo/src/app/app.module.ts
+++ 05.Modules_and_Routing/modules-and-routing-demo/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CoreModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
