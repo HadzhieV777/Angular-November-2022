dc648ff8827554973f25530dd3aa3504c0dd979c
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-16 09:26:03 +0200

    Edited global-loader.component.ts

--- 05.Modules_and_Routing/modules-and-routing-demo/src/app/core/global-loader/global-loader.component.ts
+++ 05.Modules_and_Routing/modules-and-routing-demo/src/app/core/global-loader/global-loader.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalLoaderService } from '../services/global-loader.service';
 
 @Component({
   selector: 'app-global-loader',
   templateUrl: './global-loader.component.html',
-  styleUrls: ['./global-loader.component.scss']
+  styleUrls: ['./global-loader.component.scss'],
 })
 export class GlobalLoaderComponent implements OnInit {
+  constructor(public globalLoaderService: GlobalLoaderService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
