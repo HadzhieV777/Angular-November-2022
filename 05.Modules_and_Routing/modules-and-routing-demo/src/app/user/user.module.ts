f6082853b18056167ef3ac651f08c0bb52e0e47d
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-16 15:53:05 +0200

    Generated user component

--- 05.Modules_and_Routing/modules-and-routing-demo/src/app/user/user.module.ts
+++ 05.Modules_and_Routing/modules-and-routing-demo/src/app/user/user.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListComponent } from './list/list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListComponent
+  ],
   imports: [
     CommonModule
   ]
