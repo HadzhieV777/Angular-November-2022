ce04821d20313a4992a116fe54f7d90c28a6ff53
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-17 10:46:08 +0200

    Edited app.module.ts

--- 05.Modules_and_Routing/modules-and-routing-demo/src/app/app.module.ts
+++ 05.Modules_and_Routing/modules-and-routing-demo/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     CoreModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    UserModule,
+    HttpClientModule,
+    PostModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
