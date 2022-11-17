71dd9cbc2da383a062861503208f1192805aa004
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-17 10:45:52 +0200

    Edited app.component.ts

--- 05.Modules_and_Routing/modules-and-routing-demo/src/app/app.component.ts
+++ 05.Modules_and_Routing/modules-and-routing-demo/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Test } from './test';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'modules-and-routing-demo';
