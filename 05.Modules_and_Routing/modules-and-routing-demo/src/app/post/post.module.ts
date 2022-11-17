571b82a17677c8c17b05d58804bf1374abbecf66
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-17 13:15:30 +0200

    Edited post.module.ts

--- 05.Modules_and_Routing/modules-and-routing-demo/src/app/post/post.module.ts
+++ 05.Modules_and_Routing/modules-and-routing-demo/src/app/post/post.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ListComponent } from './list/list.component';
+import { PostListComponent } from './list/list.component';
 import { Test } from '../test';
 
 @NgModule({
-  declarations: [ListComponent],
+  declarations: [PostListComponent],
   providers: [
     Test, // { provide: Test, useClass: Test }
   ],
   imports: [CommonModule],
-  exports: [ListComponent],
+  exports: [PostListComponent],
 })
 export class PostModule {}
