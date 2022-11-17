59411a757f78528326877641cc2cae51c592cffc
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-17 13:14:21 +0200

    Edited app-routing.module.ts

--- 05.Modules_and_Routing/modules-and-routing-demo/src/app/app-routing.module.ts
+++ 05.Modules_and_Routing/modules-and-routing-demo/src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostListComponent } from './post/list/list.component';
 
-const routes: Routes = [];
+import { UserListComponent } from './user/list/list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/user-list',
+  },
+  {
+    path: 'user-list', // we dont add "/" because the path is relative to the current location
+    component: UserListComponent,
+  },
+  {
+    path: 'post-list',
+    component: PostListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
