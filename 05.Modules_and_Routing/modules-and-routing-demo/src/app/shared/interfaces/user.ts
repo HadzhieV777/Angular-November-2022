4e30a39409fcac1aa2f3492c44e812e234ac8484
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-17 10:47:43 +0200

    Edited user.ts

--- 05.Modules_and_Routing/modules-and-routing-demo/src/app/shared/interfaces/user.ts
+++ 05.Modules_and_Routing/modules-and-routing-demo/src/app/shared/interfaces/user.ts
@@ -1,3 +1,21 @@
+// Differences between interface and type
+
 export interface IUser {
   username: string;
 }
+
+export type User = {
+  username: string;
+};
+
+// Interface can be implemented
+// We can assign type[] but we can't assign interface[]
+// type UserArray = User[]
+
+// we can't apply |, or other operators to interfaces
+// type UserOrSomethingElse = IUser | UserArray;
+
+// Interfaces are used if we need to extend them,
+// good for DTO, for objects from the DB
+
+// Types are used for complex operations and types
