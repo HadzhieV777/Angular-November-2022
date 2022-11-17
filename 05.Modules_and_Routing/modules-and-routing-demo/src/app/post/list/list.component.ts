da01ff01fd3b454278261c9dff14a9eb07008e9a
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-17 10:47:08 +0200

    Edited list.component.ts

--- 05.Modules_and_Routing/modules-and-routing-demo/src/app/post/list/list.component.ts
+++ 05.Modules_and_Routing/modules-and-routing-demo/src/app/post/list/list.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Test } from 'src/app/test';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private t: Test) {
+    (window as any).t1 = 1;
   }
 
+  ngOnInit(): void {}
 }
