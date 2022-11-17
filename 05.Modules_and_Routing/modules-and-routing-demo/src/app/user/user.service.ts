d8ebd5c9e849f26f186eea1036e29eaf613bb28b
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-17 10:48:57 +0200

    Edited user.service.ts

--- 05.Modules_and_Routing/modules-and-routing-demo/src/app/user/user.service.ts
+++ 05.Modules_and_Routing/modules-and-routing-demo/src/app/user/user.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { IUser } from '../shared/interfaces/index';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-// https://jsonplaceholder.typicode.com/users
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  loadUsers() {
+    return this.http.get<IUser[]>('https://jsonplaceholder.typicode.com/users');
+  }
 }
