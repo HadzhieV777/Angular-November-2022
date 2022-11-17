641651e2eda0a5b19859b07e19fc0fecc2db489e
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-17 10:48:39 +0200

    Edited user.module.ts

--- 05.Modules_and_Routing/modules-and-routing-demo/src/app/user/user.module.ts
+++ 05.Modules_and_Routing/modules-and-routing-demo/src/app/user/user.module.ts
@@ -1,15 +1,30 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
-
-
+import { Test } from '../test';
 
 @NgModule({
-  declarations: [
-    ListComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ListComponent],
+  imports: [CommonModule],
+  // providers: [
+  //   Test, // { provide: Test, useClass: Test }
+  // ],
+  exports: [ListComponent],
 })
-export class UserModule { }
+export class UserModule {
+  // forRoot stays in the highest lvl, will provide all the data
+  static forRoot(config: any): ModuleWithProviders<UserModule> {
+    return {
+      ngModule: UserModule,
+      providers: [{ provide: 'routes', useValue: config, multi: true }],
+    };
+  }
+
+  static forChild(config: any): ModuleWithProviders<UserModule> {
+    // forChild will provide only some additional data
+    return {
+      ngModule: UserModule,
+      providers: [{ provide: 'route', useValue: config, multi: true }],
+    };
+  }
+}
