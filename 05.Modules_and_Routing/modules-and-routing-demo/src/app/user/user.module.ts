285ac5cc0c837d3e75aebd41a0df20e8ec02b83a
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-18 12:51:24 +0200

    Edited user.module.ts

--- 05.Modules_and_Routing/modules-and-routing-demo/src/app/user/user.module.ts
+++ 05.Modules_and_Routing/modules-and-routing-demo/src/app/user/user.module.ts
@@ -2,10 +2,31 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserListComponent } from './list/list.component';
 import { Test } from '../test';
+import { UserDetailComponent } from './detail/detail.component';
+import { RouterModule } from '@angular/router';
+import { UserResolver } from './user-detail.resolver';
+import { AuthGuard } from './user-detail.guard';
 
 @NgModule({
-  declarations: [UserListComponent],
-  imports: [CommonModule],
+  declarations: [UserListComponent, UserDetailComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: 'user/list', // we dont add "/" because the path is relative to the current location
+        component: UserListComponent,
+      },
+      {
+        // before rendering the component(UserDetailComponent)
+        // we must wait for  resolve: { user: UserResolver } to resolve first
+        
+        path: 'user/detail/:id',
+        resolve: { user: UserResolver },
+        canActivate: [AuthGuard],
+        component: UserDetailComponent,
+      },
+    ]),
+  ],
   // providers: [
   //   Test, // { provide: Test, useClass: Test }
   // ],
