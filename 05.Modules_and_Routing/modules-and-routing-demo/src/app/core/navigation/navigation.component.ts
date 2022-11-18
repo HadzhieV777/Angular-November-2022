d83483317430a87224acbd09534042f64f0164d8
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-18 12:50:12 +0200

    Edited navigation.component.ts

--- 05.Modules_and_Routing/modules-and-routing-demo/src/app/core/navigation/navigation.component.ts
+++ 05.Modules_and_Routing/modules-and-routing-demo/src/app/core/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  navigate(path: string): void {
+    this.router.navigate([path])
+  }
 }
