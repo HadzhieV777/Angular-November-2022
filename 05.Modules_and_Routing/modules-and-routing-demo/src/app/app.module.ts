f9d1164cd2fb9b98c0c080fb52e778f4c7c61740
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-18 12:48:55 +0200

    Edited app.module.ts

--- 05.Modules_and_Routing/modules-and-routing-demo/src/app/app.module.ts
+++ 05.Modules_and_Routing/modules-and-routing-demo/src/app/app.module.ts
@@ -2,23 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+import { routes } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { StandaloneCmpComponent } from './standalone-cmp/standalone-cmp.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     CoreModule,
-    AppRoutingModule,
+    RouterModule.forRoot(routes),
     BrowserAnimationsModule,
     UserModule,
     HttpClientModule,
     PostModule,
+    StandaloneCmpComponent,
   ],
   providers: [],
   bootstrap: [AppComponent],
