f91fefce33d51b75bc7a5db211d51d85035ec2ea
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-18 12:49:11 +0200

    Edited core.module.ts

--- 05.Modules_and_Routing/modules-and-routing-demo/src/app/core/core.module.ts
+++ 05.Modules_and_Routing/modules-and-routing-demo/src/app/core/core.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { GlobalLoaderComponent } from './global-loader/global-loader.component';
 import { SharedModule } from '../shared/shared.module';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
-  declarations: [GlobalLoaderComponent],
-  imports: [CommonModule, SharedModule],
-  exports: [GlobalLoaderComponent],
+  declarations: [GlobalLoaderComponent, NavigationComponent],
+  imports: [CommonModule, SharedModule, RouterModule],
+  exports: [GlobalLoaderComponent, NavigationComponent],
 })
 export class CoreModule {}
