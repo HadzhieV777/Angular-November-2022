58ac6a65e677bd633e2f350f5642de7bc264fba9
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-20 11:26:38 +0200

    Edited app.module.ts

--- Softuni_Forum/forum/src/app/app.module.ts
+++ Softuni_Forum/forum/src/app/app.module.ts
@@ -4,31 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 // import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { ThemeListComponent } from './theme-list/theme-list.component';
 import { RecentPostsComponent } from './recent-posts/recent-posts.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MainComponent } from './main/main.component';
 import { SharedModule } from './shared/shared.module';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { UserModule } from './user/user.module';
+import { AppRoutingModule } from './app-routing.module';
+import { ThemeModule } from './theme/theme.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ThemeListComponent,
     RecentPostsComponent,
     MainComponent,
-    LoginComponent,
-    RegisterComponent,
     HomeComponent,
   ],
   imports: [
     BrowserModule,
     CoreModule,
     HttpClientModule,
-    SharedModule
-    // AppRoutingModule
+    SharedModule,
+    UserModule,
+    ThemeModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
