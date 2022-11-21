2f43a2427d393ac9b4c2598a58e8fd6857a598ec
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-21 13:39:19 +0200

    Edited home.component.ts

--- Softuni_Forum/forum/src/app/home/home.component.ts
+++ Softuni_Forum/forum/src/app/home/home.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class HomeComponent {
+  get isLogged(): boolean {
+    return this.userService.isLogged;
   }
 
+  constructor(private userService: UserService) {}
 }
