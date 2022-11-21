045f7b64f90f6ca2d837fcfdc3aee65215f545b6
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-21 13:41:41 +0200

    Edited user.module.ts

--- Softuni_Forum/forum/src/app/user/user.module.ts
+++ Softuni_Forum/forum/src/app/user/user.module.ts
@@ -4,9 +4,11 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { UserRoutingModule } from './user-routing.module';
+import { UserService } from './user.service';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent, ProfileComponent],
   imports: [CommonModule, UserRoutingModule],
+  providers: [UserService]
 })
 export class UserModule {}
