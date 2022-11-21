81e45d4ed401e98c80ca0440ee957f5c18354dab
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-21 13:42:24 +0200

    Added guards dir

---
+++ Softuni_Forum/forum/src/app/core/guards/auth.activate.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserService } from 'src/app/user/user.service';
+
+@Injectable()
+export class AuthActivate implements CanActivate {
+  constructor(private router: Router, private userService: UserService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    const { authenticationRequired, authenticationFailureRedirectUrl } =
+      route.data;
+
+    if (
+      typeof authenticationRequired === 'boolean' &&
+      authenticationRequired === this.userService.isLogged
+    ) {
+      return true;
+    }
+
+    return this.router.parseUrl(authenticationFailureRedirectUrl || '/');
+  }
+}
