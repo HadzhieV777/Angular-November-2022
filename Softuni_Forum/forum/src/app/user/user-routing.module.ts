82fc5b260ac2ad84d1eb2c61a9a676a30a58613e
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-21 13:41:26 +0200

    Edited user-routing.module.ts

--- Softuni_Forum/forum/src/app/user/user-routing.module.ts
+++ Softuni_Forum/forum/src/app/user/user-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthActivate } from '../core/guards/auth.activate';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
@@ -8,14 +9,29 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+    canActivate: [AuthActivate],
+    data: {
+      authenticationRequired: false,
+      authenticationFailureRedirectUrl: '/',
+    },
   },
   {
     path: 'register',
     component: RegisterComponent,
+    canActivate: [AuthActivate],
+    data: {
+      authenticationRequired: false,
+      authenticationFailureRedirectUrl: '/',
+    },
   },
   {
     path: 'profile',
     component: ProfileComponent,
+    canActivate: [AuthActivate],
+    data: {
+      authenticationRequired: true,
+      authenticationFailureRedirectUrl: '/login',
+    },
   },
 ];
 
