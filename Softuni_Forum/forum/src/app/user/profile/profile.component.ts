72b442099ed20f4044dfcd9c690c858bbae73d74
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-21 13:40:54 +0200

    Edited profile.component.ts

--- Softuni_Forum/forum/src/app/user/profile/profile.component.ts
+++ Softuni_Forum/forum/src/app/user/profile/profile.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class ProfileComponent{
+  user = this.userService.user;
+  
+  constructor(private userService: UserService) { }
+  
 
 }
