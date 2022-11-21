1296d808eb4f070958be979a75c46a4db201d510
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-21 13:40:17 +0200

    Edited login.component.ts

--- Softuni_Forum/forum/src/app/user/login/login.component.ts
+++ Softuni_Forum/forum/src/app/user/login/login.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  constructor(
+    private userService: UserService, 
+    private router: Router
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  login(email: string, password: string): void {
+    this.userService.login(email, password);
+    this.router.navigate(['/']);
   }
-
 }
