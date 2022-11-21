4872c3f7ac36a7a23d1cd6ba179c908325f06c70
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-21 13:41:09 +0200

    Edited register.component.ts

--- Softuni_Forum/forum/src/app/user/register/register.component.ts
+++ Softuni_Forum/forum/src/app/user/register/register.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  constructor(private userService: UserService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  register(): void {}
 }
