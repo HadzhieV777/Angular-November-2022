c866d556960cf26abb327fd90651e154b219e801
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-22 13:11:07 +0200

    Edited app.module.ts

--- Softuni_Forum/forum/src/app/app.module.ts
+++ Softuni_Forum/forum/src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-// import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { RecentPostsComponent } from './recent-posts/recent-posts.component';
 import { HttpClientModule } from '@angular/common/http';
-import { MainComponent } from './main/main.component';
 import { SharedModule } from './shared/shared.module';
-import { HomeComponent } from './home/home.component';
+// import { HomeComponent } from './home/home.component';
 import { UserModule } from './user/user.module';
 import { AppRoutingModule } from './app-routing.module';
 import { ThemeModule } from './theme/theme.module';
@@ -16,9 +14,7 @@ import { ThemeModule } from './theme/theme.module';
 @NgModule({
   declarations: [
     AppComponent,
-    RecentPostsComponent,
-    MainComponent,
-    HomeComponent,
+    // HomeComponent,
   ],
   imports: [
     BrowserModule,
