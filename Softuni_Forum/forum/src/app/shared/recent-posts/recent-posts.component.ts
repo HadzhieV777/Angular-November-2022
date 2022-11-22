a03294d9407268c4c0f1c29e8065bf2d942c7b29
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-22 13:08:19 +0200

    Refractoring

---
+++ Softuni_Forum/forum/src/app/shared/recent-posts/recent-posts.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { IPost } from '../../interfaces/index';
+
+@Component({
+  selector: 'app-recent-posts',
+  templateUrl: './recent-posts.component.html',
+  styleUrls: ['./recent-posts.component.scss'],
+})
+export class RecentPostsComponent implements OnInit {
+  postList: IPost[] | null = null;
+  errorFetcingData = false;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.loadPosts(5).subscribe({
+      next: (value) => {
+        this.postList = value;
+      },
+      error: (err) => console.log(err),
+    });
+  }
+}
