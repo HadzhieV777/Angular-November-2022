c232e37baa915359227d1eb3773fce1c18b0256b
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-22 21:32:11 +0200

    Edited app.component.ts

--- Softuni_Forum/forum/src/app/app.component.ts
+++ Softuni_Forum/forum/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import {
+  ActivationStart,
+  NavigationEnd,
+  RouteConfigLoadEnd,
+  Router,
+  RoutesRecognized,
+} from '@angular/router';
+import { filter, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'forum';
+
+  constructor(private router: Router, private pageTitle: Title) {
+    this.router.events
+      .pipe(
+        filter((e): e is ActivationStart => e instanceof ActivationStart),
+        map((e) => e.snapshot.data?.['title']),
+        filter((d) => !!d)
+      )
+      .subscribe((pageTitle) => {
+        this.pageTitle.setTitle(pageTitle);
+      });
+  }
 }
