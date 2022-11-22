b7e08f9a6257189d6ecb40d737a3b930e4022b3c
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-22 13:07:59 +0200

    Edited app-routing.module.ts

--- Softuni_Forum/forum/src/app/app-routing.module.ts
+++ Softuni_Forum/forum/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+import { HomeComponent } from './shared/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    component: PageNotFoundComponent,
   },
 ];
 
