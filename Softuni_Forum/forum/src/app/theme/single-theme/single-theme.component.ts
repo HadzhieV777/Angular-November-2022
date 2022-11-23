99f92430edc898e33efabd9a095e6922f8c710bc
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-23 12:15:14 +0200

    Edited single-theme.component.ts

--- Softuni_Forum/forum/src/app/theme/single-theme/single-theme.component.ts
+++ Softuni_Forum/forum/src/app/theme/single-theme/single-theme.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
+import { ITheme } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-single-theme',
   templateUrl: './single-theme.component.html',
   styleUrls: ['./single-theme.component.scss']
 })
-export class SingleThemeComponent implements OnInit {
+export class SingleThemeComponent {
 
-  constructor() { }
+  theme: ITheme | undefined;
 
-  ngOnInit(): void {
+  constructor(
+    private apiService: ApiService,
+    private activatedRoute: ActivatedRoute) { 
+      this.fetchTheme();
+    }
+
+  fetchTheme(): void {
+    this.theme = undefined;
+    const themeId = this.activatedRoute.snapshot.params['themeId'];
+    this.apiService.loadSingleTheme(themeId).subscribe(theme => this.theme = theme)
   }
 
 }
