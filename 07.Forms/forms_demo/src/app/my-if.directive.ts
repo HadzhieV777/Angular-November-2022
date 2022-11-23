57ab53a12025a913f145a4e96b5fe80170b2fd65
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-23 15:38:53 +0200

    Generated new directive

---
+++ 07.Forms/forms_demo/src/app/my-if.directive.ts
@@ -0,0 +1,34 @@
+import {
+  Directive,
+  Input,
+  OnChanges,
+  Optional,
+  SimpleChanges,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appMyIf]',
+})
+export class MyIfDirective implements OnChanges {
+  @Input() appMyIf = false;
+  @Input() template: TemplateRef<any> | undefined;
+
+  constructor(
+    @Optional() private templateRef: TemplateRef<any>,
+    private vc: ViewContainerRef
+  ) {}
+
+  ngOnChanges(): void {
+    if (this.appMyIf) {
+      const template = this.templateRef || this.template;
+      if (!template) {
+        return;
+      }
+      this.vc.createEmbeddedView(template, { value: 'Testing'});
+    } else {
+      this.vc.clear();
+    }
+  }
+}
