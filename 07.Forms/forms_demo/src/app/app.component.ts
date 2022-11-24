749ebfb129e552202153de58fd84a8c868f1559a
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-24 09:41:06 +0200

    Edited app.component.ts

--- 07.Forms/forms_demo/src/app/app.component.ts
+++ 07.Forms/forms_demo/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MyValueInjectionToken } from 'src/providers';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'forms_demo';
+
+  myIfValue = true;
+  
+  constructor(@Inject(MyValueInjectionToken) myValue: string) {
+    console.log(myValue);
+  }
 }
