3ff17ba3eecde30a3ce2eb3deebfe34c6f74ac92
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-24 09:41:23 +0200

    Edited app.module.ts

--- 07.Forms/forms_demo/src/app/app.module.ts
+++ 07.Forms/forms_demo/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyRouterLinkDirective } from './my-router-link.directive';
+import { MyIfDirective } from './my-if.directive';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyRouterLinkDirective,
+    MyIfDirective,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // Template driven forms
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
