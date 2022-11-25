d6dd8c32e20288eca7d0ced456e426f22b3394c6
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-25 11:30:22 +0200

    Edited register.component.ts

--- 07.Forms/forms_demo/src/app/register/register.component.ts
+++ 07.Forms/forms_demo/src/app/register/register.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+
+function createMyValidator(config: number): ValidatorFn {
+  return (control: AbstractControl) => {
+    return control.value.lenth > 10 ? { myValidator: true } : null;
+  };
+}
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  registerForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email, createMyValidator(10)]],
+    password: ['', [Validators.required, Validators.maxLength(14)]],
+  });
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  handleFormSubmit(): void {}
 }
