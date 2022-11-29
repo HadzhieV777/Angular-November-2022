c0bb3bee3d2e7c083ec7ef2ec625382e2a4911cf
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-29 14:54:43 +0200

    Edited app.module.ts

--- 09.Pipes_Interceptors_and_Subjects/demo/src/app/app.module.ts
+++ 09.Pipes_Interceptors_and_Subjects/demo/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -8,7 +9,7 @@ import { ReducePipe } from './reduce.pipe';
 
 @NgModule({
   declarations: [AppComponent, ReducePipe],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [appInterceptorProvider],
   bootstrap: [AppComponent],
 })
