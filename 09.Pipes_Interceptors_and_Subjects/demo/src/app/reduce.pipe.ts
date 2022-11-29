c308d336481c918bbd232ff3c5fdf6afc75d20e3
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-29 13:19:35 +0200

    Edited reduce pipe

--- 09.Pipes_Interceptors_and_Subjects/demo/src/app/reduce.pipe.ts
+++ 09.Pipes_Interceptors_and_Subjects/demo/src/app/reduce.pipe.ts
@@ -1,12 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'reduce'
+  name: 'reduce',
 })
-export class ReducePipe implements PipeTransform {
-
-  transform(array: any[], reduceFn: ()): unknown {
-    return null;
+export class ReducePipe<T, I> implements PipeTransform {
+  transform(
+    array: T[],
+    reduceFn: (acc: any, curr: T) => any,
+    initialValue: I
+  ): any {
+    if (initialValue !== undefined) {
+      return array?.reduce(reduceFn, initialValue);
+    }
+    return array?.reduce(reduceFn);
   }
-
 }
