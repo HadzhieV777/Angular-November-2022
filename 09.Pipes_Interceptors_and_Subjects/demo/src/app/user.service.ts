f3c755830b27739abf8a971898d2601accff594b
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-29 14:54:28 +0200

    Edited user.service.ts

--- 09.Pipes_Interceptors_and_Subjects/demo/src/app/user.service.ts
+++ 09.Pipes_Interceptors_and_Subjects/demo/src/app/user.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers() {
+    return this.http.get<any[]>('/api/users')
+  }
 }
