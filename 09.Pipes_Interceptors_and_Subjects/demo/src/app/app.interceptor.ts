602c5db6136e847909859895ba515078b5892ee5
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-29 14:16:01 +0200

    Added interceptor

---
+++ 09.Pipes_Interceptors_and_Subjects/demo/src/app/app.interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable, Provider } from '@angular/core';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+  HttpResponse,
+  HTTP_INTERCEPTORS,
+} from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+
+@Injectable()
+export class AppInterceptor implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      tap((req) => {
+        if (req instanceof HttpResponse) {
+          console.log(req.body);
+        }
+      })
+    );
+  }
+}
+
+export const appInterceptorProvider: Provider = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: AppInterceptor,
+  multi: true,
+};
