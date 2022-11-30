9a2711ad72db4e2055a504afbd5db1d81afec952
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-30 10:05:55 +0200

    Edited app.module.ts

--- 09.Pipes_Interceptors_and_Subjects/demo/src/app/app.module.ts
+++ 09.Pipes_Interceptors_and_Subjects/demo/src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { appInterceptorProvider } from './app.interceptor';
 import { ReducePipe } from './reduce.pipe';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
-  declarations: [AppComponent, ReducePipe],
+  declarations: [AppComponent, ReducePipe, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [appInterceptorProvider],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+// We don't need to add  LazyModule if we want to use lazy loading
