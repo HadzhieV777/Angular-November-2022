b25b35232edb2f2eeec03c4c6600d17c79bfb212
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-30 10:06:34 +0200

    Edited user.service.ts

--- 09.Pipes_Interceptors_and_Subjects/demo/src/app/user.service.ts
+++ 09.Pipes_Interceptors_and_Subjects/demo/src/app/user.service.ts
@@ -1,14 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  private users$$ = new BehaviorSubject<null | any>(null);
+  // private isLoading$$ = new BehaviorSubject(false);
 
-  constructor(private http: HttpClient) { }
+  users$ = this.users$$.asObservable();
+  isLoading$ = this.users$.pipe(map(users => users === null))
 
-  getUsers() {
-    return this.http.get<any[]>('/api/users')
+  constructor(private http: HttpClient) {}
+
+  loadUsers() {
+    // this.isLoading$$.next(true);
+    this.users$$.next(null)
+    this.http.get<any[]>('/api/users').subscribe({
+      next: (users) => {
+        // this.isLoading$$.next(false);
+        this.users$$.next(users);
+      },
+    });
   }
 }
