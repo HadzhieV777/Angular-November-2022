3d9c45a792319b12aaaeba29e195200ea30a5f8d
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-03 09:23:48 +0200

    Edited app module

--- Softuni_Forum/forum/src/app/app.module.ts
+++ Softuni_Forum/forum/src/app/app.module.ts
@@ -6,14 +6,18 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
-// import { HomeComponent } from './home/home.component';
 import { UserModule } from './user/user.module';
 import { AppRoutingModule } from './app-routing.module';
 import { ThemeModule } from './theme/theme.module';
+import { appInterceptorProvider } from './app.interceptor';
+import { AuthenticateComponent } from './authenticate/authenticate.component';
+import { API_ERROR } from './shared/constants';
+import { BehaviorSubject } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AuthenticateComponent,
     // HomeComponent,
   ],
   imports: [
@@ -25,7 +29,13 @@ import { ThemeModule } from './theme/theme.module';
     ThemeModule,
     AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    appInterceptorProvider,
+    {
+      provide: API_ERROR,
+      useValue: new BehaviorSubject(null)
+    }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
