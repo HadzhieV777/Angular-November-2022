1e69560fa6cffcb0322a874b875427a83bf62eca
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-03 09:25:58 +0200

    Edited error.component.ts

--- Softuni_Forum/forum/src/app/core/error/error.component.ts
+++ Softuni_Forum/forum/src/app/core/error/error.component.ts
@@ -1,19 +1,23 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
+import { BehaviorSubject, debounceTime, delay, take } from 'rxjs';
 import { API_ERROR } from 'src/app/shared/constants';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
-  styleUrls: ['./error.component.scss']
+  styleUrls: ['./error.component.scss'],
 })
 export class ErrorComponent implements OnInit {
-
   apiError$ = this.apiError.asObservable();
-  constructor(@Inject(API_ERROR) private apiError: BehaviorSubject<Error | null>) { 
 
+  constructor(
+    @Inject(API_ERROR) private apiError: BehaviorSubject<Error | null>,
+    private router: Router
+  ) {
+    this.apiError$.pipe(debounceTime(0), take(1)).subscribe(() => {
+      this.router.navigate(['/']);
+    })
   }
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
