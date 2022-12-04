0d8f5356466e93adb13f619a617c7f89e13eef5b
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-04 12:21:25 +0200

    Edited app-routing.module

--- Softuni_Forum/forum/src/app/app-routing.module.ts
+++ Softuni_Forum/forum/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { ErrorComponent } from './core/error/error.component';
 import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 import { HomeComponent } from './shared/home/home.component';
@@ -11,8 +11,18 @@ const routes: Routes = [
   },
   {
     path: 'error',
-    component: ErrorComponent
+    component: ErrorComponent,
   },
+  {
+    path: 'auth',
+    loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
+  },
+  {
+    path: 'theme',
+    loadChildren: () =>
+      import('./theme/theme.module').then((m) => m.ThemeModule),
+  },
+
   {
     path: '**',
     pathMatch: 'full',
@@ -21,7 +31,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
