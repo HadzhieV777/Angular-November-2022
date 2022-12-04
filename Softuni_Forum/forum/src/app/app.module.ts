f94af0f1156a6c007229efc45108a92caba2ba1d
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-04 12:21:38 +0200

    Edited app.module

--- Softuni_Forum/forum/src/app/app.module.ts
+++ Softuni_Forum/forum/src/app/app.module.ts
@@ -6,9 +6,7 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
-import { UserModule } from './user/user.module';
 import { AppRoutingModule } from './app-routing.module';
-import { ThemeModule } from './theme/theme.module';
 import { appInterceptorProvider } from './app.interceptor';
 import { AuthenticateComponent } from './authenticate/authenticate.component';
 import { API_ERROR } from './shared/constants';
@@ -25,8 +23,6 @@ import { BehaviorSubject } from 'rxjs';
     CoreModule,
     HttpClientModule,
     SharedModule,
-    UserModule,
-    ThemeModule,
     AppRoutingModule,
   ],
   providers: [
