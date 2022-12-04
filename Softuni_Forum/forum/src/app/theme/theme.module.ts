6ef44a12b5a588029a6e6135f87daca88a6199e2
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-04 12:23:01 +0200

    Edited theme.module

--- Softuni_Forum/forum/src/app/theme/theme.module.ts
+++ Softuni_Forum/forum/src/app/theme/theme.module.ts
@@ -6,9 +6,10 @@ import { ThemeRoutingModule } from './theme-routing.module';
 import { SingleThemeComponent } from './single-theme/single-theme.component';
 import { SharedModule } from '../shared/shared.module';
 import { FormsModule } from '@angular/forms';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
-  declarations: [ThemeListComponent, NewThemeComponent, SingleThemeComponent],
+  declarations: [ThemeListComponent, NewThemeComponent, SingleThemeComponent, MainComponent],
   imports: [CommonModule, ThemeRoutingModule, SharedModule, FormsModule],
 })
 export class ThemeModule {}
