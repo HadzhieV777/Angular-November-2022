6e98fdd8a14e6a276ebd53846f02eb2fa309b171
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-07 10:51:56 +0200

    Edited app.module.ts

--- 11.State_Management/sm-demo/src/app/app.module.ts
+++ 11.State_Management/sm-demo/src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
+import { isDevMode, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+
+import { reducers } from './+store/index';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+import { CustomSerializer } from './+store/router';
+import { EffectsModule } from '@ngrx/effects';
+import { Effects } from './+store/effects';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent, DetailsComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    StoreRouterConnectingModule.forRoot({ serializer: CustomSerializer }),
+    StoreModule.forRoot(reducers),
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([Effects]),
+    // StoreDevToolsModule.instrument({logOnly: !isDevMode() })
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
