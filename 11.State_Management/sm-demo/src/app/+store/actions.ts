9b622627e8d438f367df3fa907bcde1e23a67eae
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-07 10:50:31 +0200

    Edited actions

--- 11.State_Management/sm-demo/src/app/+store/actions.ts
+++ 11.State_Management/sm-demo/src/app/+store/actions.ts
@@ -1,7 +1,16 @@
-import { createAction } from "@ngrx/store"
+import { createAction, props } from '@ngrx/store';
 
- const actionTypes = {
-    increment:  'INCREMENT',
-}
+const actionTypes = {
+  increment: 'INCREMENT',
+  setValue: 'SET_VALUE',
+  loadUsers: 'LOAD_USERS',
+  loadUsersSuccess: 'LOAD_USERS_SUCCESS',
+  loadUsersFailure: 'LOAD_USERS_FAILURE',
+};
 
-export const increment = createAction(actionTypes.increment)
+export const increment = createAction(actionTypes.increment);
+export const setValue = createAction(actionTypes.setValue, props<{ counter: number }>());
+
+export const loadUsers = createAction(actionTypes.loadUsers, props<{ filter?: string }>());
+export const loadUsersSuccess = createAction(actionTypes.loadUsersSuccess, props<{ users: any[] }>());
+export const loadUsersFailure = createAction(actionTypes.loadUsersFailure, props<{ error: any }>());
